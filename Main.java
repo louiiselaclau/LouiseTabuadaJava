8bb58c46be6cbf5d9ab7bea200114f818b62d3bd
louiiselaclau <49662352+louiiselaclau@users.noreply.github.com>
2021-03-07 21:08:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*Louise Laclau 0050014910*/
+public class Main {
+
+ public static void main (String[] args) {
+	int contador = 1;
+	int numeroDigitado;
+	
+	System.out.println("digite o valor da tabuada:");
+		numeroDigitado = input.nextInt();
+	while (contador <=10){
+		System.out.println(numeroDigitado+" X "+contador+" = "+contador + numeroDigitado);
+		contador ++;
+	}
+
+}
+}
+
+
